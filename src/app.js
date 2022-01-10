9b33e00bf020145dcfb2ddc88a917f9775b65b70
Chris Kim <ckjr92@gmail.com>
2022-01-10 12:37:14 -0700

    on mod 3

--- src/app.js
+++ src/app.js
@@ -1,6 +1,70 @@
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
+const { allowedNodeEnvironmentFlags } = require('process');
+
+const app = new express();
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
+
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(userData);
 
 
+app.get('/', (req,res) => {
+    res.render('index', { title: 'Account Summary', accounts: accounts})
+});
+
+app.get('/savings', (req,res) => {
+    res.render('account', { account: accounts.savings })
+});
+
+app.get('/checking', (req,res) => {
+    res.render('account', { account: accounts.checking })
+});
+
+app.get('/credit', (req,res) => {
+    res.render('account', { account: accounts.credit })
+});
+
+app.get('/profile', (req,res) => {
+    res.render('profile', { user: users[0] })
+});
+
+// transfer requests
+app.get('/transfer', (req,res) => {
+    res.render('transfer')
+});
+
+app.post('/transfer', (req,res) => {
+    accounts[req.body.from].balance -= req.body.amount;
+    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+    let accountsJSON = JSON.stringify(accounts, null, 4)
+    fs.writeFileSync(path.join(__dirnmae, 'json', 'accounts.json'), accountsJSON, 'utf8');
+    res.render('transfer', { message: 'Transfer Completed'});
+});
+
+// payment reqests
+app.get('/payment', (req,res) => {
+    res.render('payment', { account: accounts.credit })
+});
+
+app.post('payment', (req,res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.avaliable += parseInt(req.body.amount);
+    let accountsJSON = JSON.stringify(accounts, null, 4)
+    fs.writeFileSync(path.join(__dirnmae, 'json', 'accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', { message: "Payment Successful", account: accounts.credit });
+});
 
 
+app.listen(3000, () => {
+    console.log("You are jamming to port 3000")
+});
